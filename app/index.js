e863b810f803b37ad505e9ba13afd5c93a7635d0
Derrick Bray <derrickbray@yahoo.com>
2017-08-14 21:29:17 -0500

    starting

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
 import 'whatwg-fetch';
+
+
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+
+
+function alert() {
+  one.style.display = 'none';
+  two.style.display = 'block';
+}
+
+one.onclick = alert;
+
+function show() {
+  two.style.display = 'none';
+  one.style.display = 'block';
+}
+
+two.onclick = show;
